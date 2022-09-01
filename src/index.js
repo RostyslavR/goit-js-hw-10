d010da21c98eb03b2d62f7ff42804744fa38860a
RostyslavR <rrmail@ukr.net>
2022-09-01 23:00:20 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+import 'notiflix/dist/notiflix-3.2.5.min.css';
 import './css/styles.css';
 import _debounce from 'debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 // ********************************300
 const DEBOUNCE_DELAY = 1000;
@@ -7,6 +9,12 @@ const DEBOUNCE_DELAY = 1000;
 const refSearchBox = document.querySelector('#search-box');
 const refCountryList = document.querySelector('.country-list');
 const refCountryInfo = document.querySelector('.country-info');
+
+Notify.init({
+  // useFontAwesome: true,
+  // useIcon: false,
+  position: 'center-top',
+});
 
 refSearchBox.addEventListener('input', _debounce(onSearchBox, DEBOUNCE_DELAY));
 
@@ -17,10 +25,12 @@ function onSearchBox(event) {
 
 function showFoundCoutries(countries) {
   if (!countries.length) {
-    console.log('no countries');
+    Notify.failure('no countries');
+    // console.log('no countries');
     return;
   }
   if (countries.length > 10) {
+    Notify.info('so many countries');
     console.log('so many countries');
     return;
   }
@@ -42,27 +52,31 @@ function showFoundCoutries(countries) {
 function makeInformationMarkup(country) {
   const { name, capital, population, flags, languages } = country;
   return (markUp = `
-  <svg width="40" height="40">
-  <use href='https://flagcdn.com/ca.svg'>${name.official}<use>
-  </svg>
-  <p>Capital: ${capital[0]}</p>
+  <img src = ${flags.svg} width = 60 />
+  <span>  ${name.official}</span>
+  <p>Capital: <span>${capital[0]}</span></p>
   <p>Population: ${population}</p>
   <p>Languages: ${languages}</p>
   `);
 }
+
 function makeListMarkup(countries) {
-  //   markUp = '';
-  //   countries.map(markUp, ({ name, flags }) => {
-  //     markUp += `
-  //   <use href='https://flagcdn.com/ca.svg'>${name.official}<use>
-  //   `;
-  //   });
-  //   return markUp;
+  console.log(countries);
+  markUp = '';
+  countries.forEach(({ name, flags }) => {
+    markUp += `
+    <li style="list-style-type: none" >
+    <img src = ${flags.svg} width = 60 />
+    <span style="font-weight: 700; font-size: 30px;" >   ${name.official}<span>
+    </li>
+    `;
+  });
+  return markUp;
 }
 
 function fetchCountries(name) {
   //********* */
-  name = 'can';
+  name = 'c';
   //  const url = `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital[0],population,flags.svg,languages`;
   const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
   fetch(url)
