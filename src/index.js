6506e2c7b5dc76269d6043a68e2d87f481a8bafd
unknown <rrmail@ukr.net>
2022-09-02 14:20:13 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import 'notiflix/dist/notiflix-3.2.5.min.css';
 import './css/styles.css';
 import _debounce from 'debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import fetchCountries from './fetchCountries';
 
-// ********************************300
-const DEBOUNCE_DELAY = 1000;
+const DEBOUNCE_DELAY = 300;
 
 const refSearchBox = document.querySelector('#search-box');
 const refCountryList = document.querySelector('.country-list');
 const refCountryInfo = document.querySelector('.country-info');
 
 Notify.init({
-  // useFontAwesome: true,
-  // useIcon: false,
   position: 'center-top',
 });
 
@@ -20,69 +18,64 @@ refSearchBox.addEventListener('input', _debounce(onSearchBox, DEBOUNCE_DELAY));
 
 function onSearchBox(event) {
   const name = event.target.value.trim();
-  fetchCountries(name);
+  fetchCountries(name).then(renderFoundCoutries).catch(notifyError);
 }
 
-function showFoundCoutries(countries) {
-  if (!countries.length) {
-    Notify.failure('no countries');
-    // console.log('no countries');
-    return;
-  }
-  if (countries.length > 10) {
-    Notify.info('so many countries');
-    console.log('so many countries');
+function renderFoundCoutries(countries) {
+  const countriesNumber = countries.length;
+  resetMarkUp();
+
+  if (!countriesNumber) {
+    Notify.failure('Oops, there is no country with that name');
     return;
   }
 
-  if (countries.length === 1) {
-    refCountryList.innerHTML = '';
+  if (countriesNumber > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+    return;
+  }
+
+  if (countriesNumber === 1) {
     refCountryInfo.innerHTML = makeInformationMarkup(countries[0]);
-    console.log('one country');
     return;
   }
-  if (countries.length < 11) {
-    refCountryInfo.innerHTML = '';
+
+  if (countriesNumber < 11 && countriesNumber > 1) {
     refCountryList.innerHTML = makeListMarkup(countries);
-    console.log('list', countries);
     return;
   }
+}
+
+function resetMarkUp() {
+  refCountryList.innerHTML = '';
+  refCountryInfo.innerHTML = '';
 }
 
 function makeInformationMarkup(country) {
   const { name, capital, population, flags, languages } = country;
   return (markUp = `
-  <img src = ${flags.svg} width = 60 />
-  <span>  ${name.official}</span>
+  <img src = ${flags.svg} width = 80 />
+  <span class="info__name">${name.official}</span>
   <p>Capital: <span>${capital[0]}</span></p>
-  <p>Population: ${population}</p>
-  <p>Languages: ${languages}</p>
+  <p>Population: <span>${population}</span></p>
+  <p>Languages: <span>${Object.values(languages).join(',')}</span></p>
   `);
 }
 
 function makeListMarkup(countries) {
-  console.log(countries);
-  markUp = '';
+  markUp = [];
   countries.forEach(({ name, flags }) => {
-    markUp += `
-    <li style="list-style-type: none" >
-    <img src = ${flags.svg} width = 60 />
-    <span style="font-weight: 700; font-size: 30px;" >   ${name.official}<span>
+    markUp.push(`
+    <li>
+    <img src = ${flags.svg} width = 40 />
+    <span class="list__name"> ${name.official}<span>
     </li>
-    `;
+    `);
   });
-  return markUp;
+  return markUp.join('');
 }
 
-function fetchCountries(name) {
-  //********* */
-  name = 'c';
-  //  const url = `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital[0],population,flags.svg,languages`;
-  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      showFoundCoutries(data);
-      console.log(data);
-    });
+function notifyError(error) {
+  Notify.failure('Error !!!');
+  console.log(error);
 }
